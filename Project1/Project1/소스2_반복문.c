921d9db6cc6d84741e16f6dc98523c269ade16ec
Chaerim Park <dasapcr@gmail.com>
2021-07-18 14:20:49 +0900

    upload file

--- Project1/Project1/소스2_반복문.c
+++ Project1/Project1/소스2_반복문.c
@@ -19,10 +19,45 @@ int main(void)
 
 
 	//3. do {} while (조건)
+	int j = 1;
+	do {
+		printf("Hello World %d\n", j++);
+	} while (j <= 10);
 
 
+	//4. 2중 반복문
+	for (int k = 1; k <= 3; k++)
+	{
+		printf("첫 번째 반복문 : %d\n", k);
 
+		for (int l = 1; l <= 5; l++)
+		{
+			printf("	두 번째 반복문 : %d\n", l);
 
+		}
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 4; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k <= i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
